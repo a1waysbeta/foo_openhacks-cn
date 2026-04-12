d2b3ab907855bfdddfa9ee0114b1900cf343e1c2
a1waysbeta <2366650060@qq.com>
2026-04-12 16:01:53 +0800

    Add files via upload

--- src/hacks_core.cpp
+++ src/hacks_core.cpp
@@ -17,16 +17,16 @@ void OpenHacksCore::Initialize()
         pfc::string8_fast errorMessage;
         if (mInitErrors & IncompatibleComponentInstalled)
         {
-            errorMessage << "\nOpenHacks is not compatible with UIHacks.";
+            errorMessage << u8"\nOpenHacks 与 UIHacks 插件不兼容。";
         }
 
         if (mInitErrors & HooksInstallError)
         {
-            errorMessage << "\nfailed to install windows hook: " << format_win32_error(mInstallHooksWin32Error) << "(0x"
+            errorMessage << u8"\n安装 Windows Hook 失败：" << format_win32_error(mInstallHooksWin32Error) << "(0x"
                          << pfc::format_hex(mInstallHooksWin32Error, 8) << ")";
         }
 
-        popup_message_v2::g_complain(core_api::get_main_window(), "OpenHacks init failed", errorMessage);
+        popup_message_v2::g_complain(core_api::get_main_window(), u8"OpenHacks 初始化失败", errorMessage);
         return;
     }
 
