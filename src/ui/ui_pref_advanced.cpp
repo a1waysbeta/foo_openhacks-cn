69981257e480460f0e87f8028292a06e1b4916ec
a1waysbeta <2366650060@qq.com>
2026-09-09 09:46:18 +0800

    Add files via upload

--- src/ui/ui_pref_advanced.cpp
+++ src/ui/ui_pref_advanced.cpp
@@ -1,23 +1,23 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "ui_pref_advanced.h"
 #include "preferences_page_impl.h"
 #include "hacks_core.h"
 #include "hacks_vars.h"
 #include "hacks_guids.h"
 
-DECLARE_PREFERENCES_PAGE(u8"高级", UIPrefAdvancedDialog, 100.0, OpenHacksGuids::kAdvancedPageGuid, OpenHacksGuids::kDUIPageGuid);
+DECLARE_PREFERENCES_PAGE("Advanced", UIPrefAdvancedDialog, 100.0, OpenHacksGuids::kAdvancedPageGuid, OpenHacksGuids::kDUIPageGuid);
 
 void UIPrefAdvancedDialog::OnInitDialog()
 {
     SetHeaderFont(IDC_PREF_HEADER1);
 
     mComboMenuBar.Attach(GetDlgItem(IDC_MENUBAR));
-    mComboMenuBar.AddString(TEXT("显示"));
-    mComboMenuBar.AddString(TEXT("隐藏"));
+    mComboMenuBar.AddString(TEXT("Show"));
+    mComboMenuBar.AddString(TEXT("Hide"));
 
     mComboStatusBar.Attach(GetDlgItem(IDC_STATUSBAR));
-    mComboStatusBar.AddString(TEXT("显示"));
-    mComboStatusBar.AddString(TEXT("隐藏"));
+    mComboStatusBar.AddString(TEXT("Show"));
+    mComboStatusBar.AddString(TEXT("Hide"));
 
     LoadUIState();
 }
@@ -34,18 +34,24 @@ void UIPrefAdvancedDialog::OnCommand(UINT code, int id, CWindow ctrl)
     {
         NotifyStateChanges(true);
     }
+    else if (code == BN_CLICKED && id == IDC_PREF_DPI_BOOST)
+    {
+        NotifyStateChanges(true);
+    }
 }
 
 void UIPrefAdvancedDialog::LoadUIState()
 {
     mComboMenuBar.SetCurSel(OpenHacksVars::ShowMainMenu ? 0 : 1);
     mComboStatusBar.SetCurSel(OpenHacksVars::ShowStatusBar ? 0 : 1);
+    uButton_SetCheck(m_hWnd, IDC_PREF_DPI_BOOST, OpenHacksVars::PreferencesDPIBoost);
 }
 
 void UIPrefAdvancedDialog::SaveUIState()
 {
     OpenHacksVars::ShowMainMenu = mComboMenuBar.GetCurSel() == 1 ? false : true;
     OpenHacksVars::ShowStatusBar = mComboStatusBar.GetCurSel() == 1 ? false : true;
+    OpenHacksVars::PreferencesDPIBoost = uButton_GetCheck(m_hWnd, IDC_PREF_DPI_BOOST);
 }
 
 void UIPrefAdvancedDialog::ApplySettings()
@@ -53,4 +59,9 @@ void UIPrefAdvancedDialog::ApplySettings()
     auto& api = OpenHacksCore::Get();
     api.ShowOrHideMenuBar(OpenHacksVars::ShowMainMenu);
     api.ShowOrHideStatusBar(OpenHacksVars::ShowStatusBar);
+
+    // The DPI boost hook is always installed (when foo_openhacks loaded
+    // successfully); the runtime override is gated by this flag, so simply
+    // flipping the config takes effect the next time Preferences is opened.
+    // No need to call any install/uninstall API here.
 }
